7a1ad801891857bd1fbf8a6074f5a847d88f209a
Washington Mendes <washington.mendes@zigpay.com.br>
2021-11-07 00:54:42 -0300

    Challenge finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
+import { ReactElement } from "react";
+
+import '../styles/content.scss';
+interface ContentProps {
+  children: ReactElement;
+  title: string;
+}
+export function Content({ children, title }: ContentProps) {
   // Complete aqui
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">{children}</div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
+import { ReactElement } from "react";
+import "../styles/sidebar.scss";
+interface SideBarProps {
+  children: ReactElement;
+}
+
+export function SideBar({ children }: SideBarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+      {children}
+    </nav>
+  );
 }
